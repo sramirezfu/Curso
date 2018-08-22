428eb57c6f5cd742c2617c96d69f5248a2cadd3a
sramirezfu <santiagostiven@hotmail.com>
2018-08-21 21:24:34 -0500

    21/08/2018

--- Proyecto/db/schema.rb
+++ Proyecto/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815020100) do
+ActiveRecord::Schema.define(version: 20180822011943) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "likes_counter"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
