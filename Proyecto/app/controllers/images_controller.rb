860343b394437f2b9b92f16f5d34550b59cea302
sramirezfu <santiagostiven@hotmail.com>
2018-08-23 21:31:54 -0500

    CRUD

--- Proyecto/app/controllers/images_controller.rb
+++ Proyecto/app/controllers/images_controller.rb
@@ -1,5 +1,49 @@
 class ImagesController < ApplicationController
-    def new 
+
+    before_action :set_image, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @images = Image.all            # variable de tipo array @images
     end
+
+    def new 
+        @image = Image.new
+    end
+
+    def crete 
+        #render plain: params[:image].inspect
+        @image = Image.new images_params
+        @image.save
+        redirect_to @image #redirecciona a una nueva vista
+    end  
+
+    def show       
+    end
+        
+    def edit	    
+    end
+
+    def update
+        # buscamos nuestro id del objeto imagen
+        @image.update images_params 
+        redirect_to @image
+    end
+
+    def destroy        
+        @image.destroy
+        redirect_to images_path 
+    end
+
+    private
+
+    def images_params
+        params.require(:image).permit(:description)
+    end 
+
+    def set_image
+        @image = Image.find params[:id]
+    end
+   
+      
  
 end
